14e6119c9a8d8939a25d49719d87592bbb7bf4b5
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-10-02 19:12:28 -0400

    added base entities for products

--- GreatwideApp.UI/Controllers/HomeController.cs
+++ GreatwideApp.UI/Controllers/HomeController.cs
@@ -5,14 +5,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GreatwideApp.UI.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
+using GreatwideApp.Infrastructure.Data;
 
 namespace GreatwideApp.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            return Json(_dbContext.Product.ToList());
+            //return View();
         }
 
         public IActionResult Privacy()
@@ -23,6 +35,10 @@ namespace GreatwideApp.UI.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            _logger.LogError("Error occurred during request.", exception.Path, exception.Error.Message, exception.Error.StackTrace);
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
